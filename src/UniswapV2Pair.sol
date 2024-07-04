b056430f837db23f00a9fc2c75917c20c6b06a11
Luke Sheppard <sheppiegit@gmail.com>
2024-07-03 21:28:16 -0400

    initial setup for minting tokens.  Need to add errors, events, Math lib, token0, token1, etc.

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -1,6 +1,41 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+import {ERC20} from "../lib/solmate/src/tokens/ERC20.sol";
 
-contract UniswapV2Pair {
+interface IERC20 {
+    function balanceOf(address) external returns (uint256);
+    function transfer(address to, uint256 amount) external;
+}
 
+contract UniswapV2Pair is ERC20{
+
+    uint256 private reserve0;
+    uint256 private reserve1;
+
+    function mint() public {
+        uint256 balance0 = IERC20(token0).balanceOf(address(this));
+        uint256 balance1 = IERC20(token1).balanceOf(address(this));
+
+        uint256 amount0 = balance0 - reserve0;
+        uint256 amount1 = balance1 - reserve1;
+
+        uint256 liquidity;
+
+        if (totalSupply == 0){
+            liquidity = Math.sqrt(amount0 * amount1) - MINIMUM_LIQUIDITY;
+            _mint(address(0), MINIMUM_LIQUIDITY);
+        }else{
+            liquidity = Math.min(
+                (amount0 *totalSupply) / _reserve0,
+                (amount1 * totalSupply) / _reserve1
+            );
+        }
+
+        if (liquidity <= 0) revert InsufficientLiquidityMinted();
+
+        _mint(msg.sender, liquidity);
+        _update(balance0, balance0);
+
+        emit Mint(msg.sender, amount0, amount1);
+    }
 }
