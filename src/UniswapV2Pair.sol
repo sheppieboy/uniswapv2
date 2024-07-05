c71dac410c0e133395261bcd92d2a9beace66e87
Luke Sheppard <sheppiegit@gmail.com>
2024-07-05 14:07:43 -0400

    fixed logic error in burn method that was burning from message sender rather than the contract.

--- src/UniswapV2Pair.sol
+++ src/UniswapV2Pair.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 import {ERC20} from "../lib/solmate/src/tokens/ERC20.sol";
+import {Math} from "./libraries/Math.sol";
 
 interface IERC20 {
     function balanceOf(address) external returns (uint256);
@@ -11,7 +12,7 @@ error InsufficientLiquidityMinted();
 error InsufficientLiquidityBurned();
 error TransferFailed();
 
-contract UniswapV2Pair is ERC20{
+contract UniswapV2Pair is ERC20, Math{
     uint256 constant MINIMUM_LIQUIDITY = 1000;
 
     uint256 private reserve0;
@@ -20,7 +21,7 @@ contract UniswapV2Pair is ERC20{
     address public token0;
     address public token1;
 
-    event Burn(address indexed sender, uint256 amount0, uint256 amount1);
+    event Burn(address indexed sender, uint256 amount0, uint256 amount1, address to);
     event Mint(address indexed sender, uint256 amount0, uint256 amount1);
     event Sync(uint256 reserve0, uint256 reserve1);
 
@@ -56,27 +57,27 @@ contract UniswapV2Pair is ERC20{
         emit Mint(msg.sender, amount0, amount1);
     }
 
-    function burn() public {
+    function burn(address to) public returns (uint256 amount0, uint256 amount1) {
         uint256 balance0 = IERC20(token0).balanceOf(address(this));
         uint256 balance1 = IERC20(token1).balanceOf(address(this));
 
-        uint256 liquidity = balanceOf[msg.sender];
+        uint256 liquidity = balanceOf[address(this)];
 
-        uint256 amount0 = (liquidity * balance0)/totalSupply;
-        uint256 amount1 = (liquidity * balance1)/totalSupply;
+        amount0 = (liquidity * balance0)/totalSupply;
+        amount1 = (liquidity * balance1)/totalSupply;
 
-        if (amount0 <= 0 || amount1 <= 0) revert InsufficientLiquidityBurned();
+        if (amount0 == 0 || amount1 == 0) revert InsufficientLiquidityBurned();
 
-        _burn(msg.sender, liquidity);
+        _burn(address(this), liquidity);
 
-        _safeTransfer(token0, msg.sender, amount0);
-        _safeTransfer(token1, msg.sender, amount1);
+        _safeTransfer(token0, to, amount0);
+        _safeTransfer(token1, to, amount1);
         balance0 = IERC20(token0).balanceOf(address(this));
         balance1 = IERC20(token1).balanceOf(address(this));
 
         _update(balance0, balance1);
 
-        emit Burn(msg.sender, amount0, amount1);
+        emit Burn(msg.sender, amount0, amount1, to);
     }
 
 
