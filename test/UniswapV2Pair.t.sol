8fa3c0152fd1a6949b34cd78aa979d03e61d1788
Luke Sheppard <sheppiegit@gmail.com>
2024-07-06 16:52:57 -0400

    test contract to remove and add liq.

--- test/UniswapV2Pair.t.sol
+++ test/UniswapV2Pair.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.13;
 import {Test, console} from "forge-std/Test.sol";
 import {ERC20Mintable} from "./mocks/ERC20Mintable.sol";
 import {UniswapV2Pair} from "../src/UniswapV2Pair.sol";
+import {ERC20} from "../lib/solmate/src/tokens/ERC20.sol";
 
 contract UniswapV2PairTest is Test{
     ERC20Mintable token0;
@@ -18,5 +19,19 @@ contract UniswapV2PairTest is Test{
 
         // token0.mint(10 ether);
         // token1.mint(10 ether);
+    }
+}
+
+contract TestContractiveContract{
+
+    function addLiquidity(address _pairAddress, address _token0, address _token1, uint256 amount0, uint256 amount1) public {
+        ERC20(_token0).transfer(_pairAddress, amount0);
+        ERC20(_token1).transfer(_pairAddress, amount1);
+
+        UniswapV2Pair(_pairAddress).mint();
+    }
+
+    function removeLiquidity(address _pairAddress) public {
+        UniswapV2Pair(_pairAddress).burn();
     }
 }
