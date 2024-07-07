6dd65e2b43aaeda99001f475a626ff0a7cc96523
Luke Sheppard <sheppiegit@gmail.com>
2024-07-07 17:40:38 -0400

    Router testing contract setup.

---
+++ test/UniswapV2Router.t.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+import {Test, console} from "forge-std/Test.sol";
+import {UniswapV2Factory} from "../src/UniswapV2Factory.sol";
+import {ERC20Mintable} from "./mocks/ERC20Mintable.sol";
+import {UniswapV2Router} from "../src/UniswapV2Router.sol";
+
+contract UniswapV2RouterTest {
+    UniswapV2Factory factory;
+
+    UniswapV2Router router;
+
+    ERC20Mintable tokenA;
+    ERC20Mintable tokenB;
+
+    function setUp() public {
+        factory = new UniswapV2Factory();
+        router = new UniswapV2Router();
+
+        tokenA = new ERC20Mintable("Token A", "A");
+        tokenB = new ERC20Mintable("Token B", "B");
+
+        tokenA.mint(20 ether, address(this));
+        tokenB.mint(20 ether, address(this));
+    }
+}
