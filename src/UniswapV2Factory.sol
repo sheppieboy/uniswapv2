d671707ddfe9ee6e61c179083af68df4166ae8cb
Luke Sheppard <sheppiegit@gmail.com>
2024-07-07 13:25:23 -0400

    The UniswapV2 Factory contract that keeps track of all UniswapV2Pairs that are created.  It will deploy a new pair when called by using the create2 opcode.

---
+++ src/UniswapV2Factory.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {UniswapV2Pair} from "./UniswapV2Pair.sol";
+// import {IUniswapV2} from "./interfaces/IUniswapV2Pair.sol"; //not yet created
+
+contract UniswapV2Factory {
+    mapping(address => mapping(address => address)) public pairs;
+    address[] public allPairs;
+
+    function createPair(address tokenA, address tokenB) public returns (address pair){
+
+        if (tokenA == tokenB) revert IdenticalAddresses();
+
+        (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+
+        if (token0 == address(0)) revert ZeroAddress();
+
+        if (pairs[token0][token1] != address(0)) revert PairExists();
+
+        //get creation bytecide of UniswapV2Pair, it includes:
+        // constructor logic, responsible for smart contract initialization and deployment, NOT stored on the blockchain
+        // runtime bytecode, which is actual business logic of contract, it's this bytecode thats store on the blockchain
+        bytes memory bytecode = type(UniswapV2Pair).creationCode; 
+        bytes salt = keccak256(abi.encodePacked(token0, token1)); //sequence of bytes thats used to generate new contract's address deterministically
+        assembly {
+            //create a new address deterministically using bytecode + salt
+            //deploy a new UniswapV2Pair contract
+            //get that pair's address
+            pair := create2(0, add(bytecode, 32), mload(bytecode), salt)
+        }
+
+        IUniswapV2Pair(pair).initialize(token0, token1);
+
+        pairs[token0][token1] = pair;
+        pairs[token1][token0] = pair;
+
+        allPairs.push(pair);
+
+        emit PairCreated(token0, token1, pair, allPairs.length);
+
+    }
+}
