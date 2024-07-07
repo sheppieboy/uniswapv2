60d517eb8386bd6be94043d97e0cee6e9980fc9d
Luke Sheppard <sheppiegit@gmail.com>
2024-07-07 15:35:32 -0400

    initial setup for testing UniswapV2Factory contract.

---
+++ test/UniswapV2Factor.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+import {Test, console} from "forge-std/Test.sol";
+import {UniswapV2Factory} from "../src/UniswapV2Factory.sol";
+import {ERC20Mintable} from "./mocks/ERC20Mintable.sol";
+contract UniswapV2FactoryTest is Test {
+    UniswapV2Factory factory;
+
+    ERC20Mintable token0;
+    ERC20Mintable token1;
+    ERC20Mintable token2;
+    ERC20Mintable token3;
+
+    function setUp() public {
+        factory = new UniswapV2Factory();
+
+        token0 = new ERC20Mintable("TokenA", "A");
+        token1 = new ERC20Mintable("TokenB", "B");
+        token2 = new ERC20Mintable("TokenC", "C");
+        token3 = new ERC20Mintable("Tokend", "D");
+    }
+}
