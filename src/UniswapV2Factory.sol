372198496fe3fda543fd0352433c1fefbea90fa5
Luke Sheppard <sheppiegit@gmail.com>
2024-07-07 13:37:17 -0400

    updated and added the errors, the pair created event and fixed a small mistake with the salt being bytes32 not bytes.

--- src/UniswapV2Factory.sol
+++ src/UniswapV2Factory.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.13;
 
 import {UniswapV2Pair} from "./UniswapV2Pair.sol";
-// import {IUniswapV2} from "./interfaces/IUniswapV2Pair.sol"; //not yet created
+import {IUniswapV2Pair} from "./interfaces/IUniswapV2Pair.sol";
+
+error IdenticalAddresses();
+error ZeroAddress();
+error PairExists();
 
 contract UniswapV2Factory {
     mapping(address => mapping(address => address)) public pairs;
     address[] public allPairs;
+
+    event PairCreated(address indexed token0, address token1, address pair, uint256);
 
     function createPair(address tokenA, address tokenB) public returns (address pair){
 
@@ -22,7 +28,7 @@ contract UniswapV2Factory {
         // constructor logic, responsible for smart contract initialization and deployment, NOT stored on the blockchain
         // runtime bytecode, which is actual business logic of contract, it's this bytecode thats store on the blockchain
         bytes memory bytecode = type(UniswapV2Pair).creationCode; 
-        bytes salt = keccak256(abi.encodePacked(token0, token1)); //sequence of bytes thats used to generate new contract's address deterministically
+        bytes32 salt = keccak256(abi.encodePacked(token0, token1)); //sequence of bytes thats used to generate new contract's address deterministically
         assembly {
             //create a new address deterministically using bytecode + salt
             //deploy a new UniswapV2Pair contract
