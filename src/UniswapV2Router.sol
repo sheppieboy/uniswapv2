469743ce0eb7f45d9b19b596381e1067c2ab3388
Luke Sheppard <sheppiegit@gmail.com>
2024-07-07 14:02:43 -0400

    addLiquidity function call to check if there is a pair or create a pair, mint LP tokens to the provided address in the function call.

--- src/UniswapV2Router.sol
+++ src/UniswapV2Router.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 import {IUniswapV2Factory} from "./interfaces/IUniswapV2Factory.sol";
+import {IUniswapV2Pair} from "./interfaces/IUniswapV2Pair.sol";
 
 contract UniswapV2Router {
     IUniswapV2Factory factory;
@@ -8,4 +9,32 @@ contract UniswapV2Router {
     constructor(address factoryAddress) {
         factory = IUniswapV2Factory(factoryAddress);
     }
+
+
+    function addLiquidity(
+        address tokenA, 
+        address tokenB,
+        //amounts we want to deposit into the pair, its an upper bound
+        uint256 amountADesired, 
+        uint256 amountBDesired,
+        //min amounts we want to deposit, Pair contract always issues smaller amounts of LP tokens when we deposit unbalance liquidity
+        // so min params all us to control how much liquidity we're ready to lose
+        uint256 amountAMin,
+        uint256 amountBMin,
+        //address to is the address that recieves the LP tokens
+        address to) 
+        public returns (uint256 amountA, uint256 amountB, uint256 liquidity){
+            
+            if (factory.pairs(tokenA, tokenB) == address(0)) {
+                factory.createPair(tokenA, tokenB);
+            }
+
+            (amountA, amountB) = _calculateLiquidity(tokenA, tokenB, amountADesired, amountBDesired, amountAMin, amountBMin);
+
+            address pairAddress = UniswapV2Library.pairFor(address(factory), tokenA, tokenB);
+
+            _safeTransferFrom(tokenA, msg.sender, pairAddress, amountA);
+            _safeTransferFrom(tokenB, msg.sender, pairAddress, amountB);
+            liquidity = IUniswapV2Pair(pairAddress).mint(to);
+        }
 }
