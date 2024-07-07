f823b794e52df92e0bd14b53bd8cfbc752abaad5
Luke Sheppard <sheppiegit@gmail.com>
2024-07-07 13:44:54 -0400

    added the interface for the factory contract to be used by the UniswapV2Router contract.

--- src/UniswapV2Router.sol
+++ src/UniswapV2Router.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+import {IUniswapV2Factory} from "./interfaces/IUniswapV2Factory.sol";
 
 contract UniswapV2Router {
     IUniswapV2Factory factory;
---
+++ src/interfaces/IUniswapV2Factory.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+interface IUniswapV2Factory {
+    function pairs(address, address) external pure returns (address);
+
+    function createPair(address, address) external pure returns (address);
+}
