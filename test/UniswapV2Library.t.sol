7d92afed0a0742a71073b3840888cb7bf9897c58
Luke Sheppard <sheppiegit@gmail.com>
2024-07-10 14:16:00 -0400

    tests for the UniswapV2Library function getAmountsOut.

--- test/UniswapV2Library.t.sol
+++ test/UniswapV2Library.t.sol
@@ -12,21 +12,37 @@ contract UniswapV2LibraryTest is Test {
 
     ERC20Mintable tokenA;
     ERC20Mintable tokenB;
+    ERC20Mintable tokenC;
+    ERC20Mintable tokenD;
 
     UniswapV2Pair pair;
+    UniswapV2Pair pair2;
+    UniswapV2Pair pair3;
 
     function setUp() public {
         factory = new UniswapV2Factory();
 
         tokenA = new ERC20Mintable("TokenA", "A");
         tokenB = new ERC20Mintable("TokenB", "B");
+        tokenC = new ERC20Mintable("TokenC", "C");
+        tokenD = new ERC20Mintable("TokenD", "D");
 
         tokenA.mint(10 ether, address(this));
         tokenB.mint(10 ether, address(this));
+        tokenC.mint(10 ether, address(this));
+        tokenD.mint(10 ether, address(this));
 
         address pairAddress = factory.createPair(address(tokenA), address(tokenB));
 
         pair = UniswapV2Pair(pairAddress);
+
+        pairAddress = factory.createPair(address(tokenB), address(tokenC));
+        
+        pair2 = UniswapV2Pair(pairAddress);
+
+        pairAddress = factory.createPair(address(tokenC), address(tokenD));
+
+        pair3 = UniswapV2Pair(pairAddress);
     }
 
     function encodeError(string memory error) internal pure returns (bytes memory encoded){
@@ -114,6 +130,43 @@ contract UniswapV2LibraryTest is Test {
     function test_RevertWhen_GetOutAmountWithZeroReserveOut() public {
         vm.expectRevert(encodeError("InsufficientLiquidity()"));
         UniswapV2Library.getAmountOut(1000, 1.5 ether, 0 ether);
+    }
+
+    function test_GetAmountsOut() public {
+        tokenA.transfer(address(pair), 1 ether);
+        tokenB.transfer(address(pair), 2 ether);
+        pair.mint(address(this));
+
+
+        tokenB.transfer(address(pair2), 1 ether);
+        tokenC.transfer(address(pair2), 0.5 ether);
+        pair2.mint(address(this));
+
+        tokenC.transfer(address(pair3), 1 ether);
+        tokenD.transfer(address(pair3), 2 ether);
+        pair3.mint(address(this));
+
+        address[] memory path = new address[](4);
+        path[0] = address(tokenA);
+        path[1] = address(tokenB);
+        path[2] = address(tokenC);
+        path[3] = address(tokenD);
+
+        uint256[] memory amounts = UniswapV2Library.getAmountsOut(address(factory), 0.1 ether, path);
+
+        assertEq(amounts.length, 4);
+        assertEq(amounts[0], 0.1 ether);
+        assertEq(amounts[1], 0.181322178776029826 ether);
+        assertEq(amounts[2], 0.076550452221167502 ether);
+        assertEq(amounts[3], 0.141817942760565270 ether);
+    }
+
+    function test_RevertWhen_GetAmountsOutInvalidPath() public {
+        address[] memory path = new address[](1);
+        path[0] = address(tokenA);
+
+        vm.expectRevert(encodeError("InvalidPath()"));
+        UniswapV2Library.getAmountsOut(address(factory), 0.1 ether, path);
     }
 
 
